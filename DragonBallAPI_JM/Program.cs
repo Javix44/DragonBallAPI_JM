bb1b9c2e739f0fd80a1057856d38872c4161ab3d
Javier Martinez <javiermartinezv33@gmail.com>
2025-04-09 10:40:09 -0600

    Swagger update to support authorization

--- DragonBallAPI_JM/Program.cs
+++ DragonBallAPI_JM/Program.cs
@@ -39,7 +39,37 @@ builder.Services.AddHttpClient();
 
 // Configuración de Swagger/OpenAPI para la documentación de la API.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DragonBall API", Version = "v1" });
+
+    // Agregar el esquema de seguridad para JWT Bearer
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        Description = "Ingrese el token JWT en el formato 'Bearer {token}'"
+    });
+
+    // Requerir el esquema de seguridad en todas las operaciones
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 var app = builder.Build();
 
